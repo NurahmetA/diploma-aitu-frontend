af9d38dd75c8caa92cef2a752fe084d3450b7045
beibarys.zholmyrza <beibarys.zholmyrza@btsdigital.kz>
2022-06-09 23:04:10 +0600

    added: student dashboard page

--- src/layouts/main-content.layout.js
+++ src/layouts/main-content.layout.js
@@ -17,6 +17,8 @@ import CommissionGradesPage from "../pages/commission-pages/grades.page";
 import SecretaryGradesPage from "../pages/secretary/grades.page";
 import StudentDashboard from "../pages/student-pages/dashboard.page";
 import StudentTeamPage from "../pages/student-pages/team.page";
+import StudentTopicPage from "../pages/student-pages/topic.page";
+import StudentDashboardPage from "../pages/student-pages/dashboard.page";
 
 function MainContentLayout(props) {
     return (
@@ -38,10 +40,9 @@ function MainContentLayout(props) {
                 <Route path="/secretary/defence/grades/:id" element={<SecretaryGradesPage/>}/>
                 <Route path="/secretary/documents" element={<SecretaryDocumentsPage/>}/>
                 <Route path="/secretary/documents/:id" element={<SecretaryDocumentPage/>}/>
-                <Route path="/student/dashboard" element={<StudentDashboard/>}/>
+                <Route path="/student/dashboard" element={<StudentDashboardPage/>}/>
                 <Route path="/student/team" element={<StudentTeamPage/>}/>
-                <Route path="/student/dashboard" element={<StudentDashboard/>}/>
-                <Route path="/student/dashboard" element={<StudentDashboard/>}/>
+                <Route path="/student/topic" element={<StudentTopicPage/>}/>
             </Routes>
         </div>
     );
